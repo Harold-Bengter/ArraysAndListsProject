1b7df4f0fecc61114a22d154b8c39b6597120f9e
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-11 23:31:29 -0400

    Finished all Todos

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,14 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            List<int> evenList = new List<int>();
+            List<int> oddList = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +25,34 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            for (int i = 1; i < 11; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenList.Add(i);
+                }
+                else if  (i % 2 == 1) 
+                {
+                    oddList.Add(i);
+                }
 
-            /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+                /* Now using foeach or for loops,
+                 * display each List of even and odd numbers
+                 *
+                 * Try to be creative in your display
+                 */
+                Console.WriteLine("These are all the evens");
+                foreach (int number in evenList)
+                {
+                    Console.WriteLine($"{number} is an even number");
+                }
+
+                Console.WriteLine("All the odds");
+                foreach (int number in oddList)
+                {
+                    Console.WriteLine($"{number} is an odd number");
+                }
+            }
         }
     }
 }
